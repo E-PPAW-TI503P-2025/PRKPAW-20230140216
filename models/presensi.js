3da4e03f56d402740d2d93f35068759faa974eff
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-24 17:02:44 +0700

    feat: Create Presensi migration and model for attendance tracking

---
+++ models/presensi.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Presensi extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Presensi.init({
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Presensi',
+  });
+  return Presensi;
+};
