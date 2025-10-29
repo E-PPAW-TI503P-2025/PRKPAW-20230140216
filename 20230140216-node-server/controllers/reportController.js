27e2b51a491ebfca5ee1f7bfe70177104b505d3f
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-29 09:10:21 +0700

    nambah operator sequelize

--- 20230140216-node-server/controllers/reportController.js
+++ 20230140216-node-server/controllers/reportController.js
@@ -1,4 +1,7 @@
 const presensiRecords = require("../data/presensiData");
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
+
 exports.getDailyReport = (req, res) => {
   console.log("Controller: Mengambil data laporan harian dari array...");
   res.json({
