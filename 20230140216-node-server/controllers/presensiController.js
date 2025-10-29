d33ce7d61b115a69dabac9250da54ff35e5c0a1e
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-29 09:06:34 +0700

    nambah updatePresensi

--- 20230140216-node-server/controllers/presensiController.js
+++ 20230140216-node-server/controllers/presensiController.js
@@ -46,6 +46,40 @@ exports.CheckIn = async (req, res) => {
   }
 };
 
+exports.updatePresensi = async (req, res) => {
+  try {
+    const presensiId = req.params.id;
+    const { checkIn, checkOut, nama } = req.body;
+    if (checkIn === undefined && checkOut === undefined && nama === undefined) {
+      return res.status(400).json({
+        message:
+          "Request body tidak berisi data yang valid untuk diupdate (checkIn, checkOut, atau nama).",
+      });
+    }
+    const recordToUpdate = await Presensi.findByPk(presensiId);
+    if (!recordToUpdate) {
+      return res
+        .status(404)
+        .json({ message: "Catatan presensi tidak ditemukan." });
+    }
+
+    recordToUpdate.checkIn = checkIn || recordToUpdate.checkIn;
+    recordToUpdate.checkOut = checkOut || recordToUpdate.checkOut;
+    recordToUpdate.nama = nama || recordToUpdate.nama;
+    await recordToUpdate.save();
+
+    res.json({
+      message: "Data presensi berhasil diperbarui.",
+      data: recordToUpdate,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan pada server", error: error.message });
+  }
+};
+
+
 exports.deletePresensi = async (req, res) => {
   try {
     const { id: userId } = req.user;
