7a39fca49efb4945bc65718df0330023651da0a9
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-29 09:04:50 +0700

    nambah deletePresensi

--- 20230140216-node-server/controllers/presensiController.js
+++ 20230140216-node-server/controllers/presensiController.js
@@ -46,6 +46,32 @@ exports.CheckIn = async (req, res) => {
   }
 };
 
+exports.deletePresensi = async (req, res) => {
+  try {
+    const { id: userId } = req.user;
+    const presensiId = req.params.id;
+    const recordToDelete = await Presensi.findByPk(presensiId);
+
+    if (!recordToDelete) {
+      return res
+        .status(404)
+        .json({ message: "Catatan presensi tidak ditemukan." });
+    }
+    if (recordToDelete.userId !== userId) {
+      return res
+        .status(403)
+        .json({ message: "Akses ditolak: Anda bukan pemilik catatan ini." });
+    }
+    await recordToDelete.destroy();
+    res.status(204).send();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Terjadi kesalahan pada server", error: error.message });
+  }
+};
+
+
 exports.CheckOut = async (req, res) => {
   // Gunakan try...catch
   try {
